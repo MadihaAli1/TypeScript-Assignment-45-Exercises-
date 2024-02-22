0ab73c173a75393f56aaa87c64818ef92ecb3374
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 18:56:25 +0500

    Add files via upload

---
+++ exercise_5.ts
@@ -0,0 +1,7 @@
+/* 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called 
+famous_person. Then compose your message and store it in a new variable called message. Print your message.*/
+
+let famous_person= "Mark Twain,";
+let message= " 'To succeed life,youneed two things ingonrance confidence' ";
+console.log(famous_person+message);
+
