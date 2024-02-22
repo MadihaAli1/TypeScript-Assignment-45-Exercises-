9c207e13abdd13c24787d09a0417cdd8b4b2d757
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:53:31 +0500

    Delete exercise_20.js
    Mistake

--- exercise_20.js
+++
@@ -1,5 +0,0 @@
-/*Think of something you could store in a array. For example, you could make a list of mountains,
-rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list
-containing these items.*/
-var countries = ["Italy", "Japan", "France", "Turkey", "London"];
-console.log("list of countries: " + countries);
