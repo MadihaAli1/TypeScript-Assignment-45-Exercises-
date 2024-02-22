f07fabe33b3af2180c1408014664fec3b65fbbd1
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 18:08:25 +0500

    Delete exercise_2.js

--- exercise_2.js
+++
@@ -1,5 +0,0 @@
-/* 2. Personal Message: Store a person’s name in a variable, and print a message to that person.
-Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
-var person = "Madiha";
-var persnol_Message = "Hello";
-console.log(persnol_Message + person);
