d492f1d9e1c79be7eb66836d39407826ff4337d7
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:53:01 +0500

    Delete exercise_2.ts
    Mistake

--- exercise_2.ts
+++
@@ -1,6 +0,0 @@
-/*Personal Message: Store a person’s name in a variable, and print a message to that person. 
-Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
-
-let person="Madiha";
- let persnol_Message="Hello"
- console.log(persnol_Message+person);
