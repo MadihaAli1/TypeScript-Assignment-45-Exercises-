a0e326f089da975d55373ab870e7723a8705a8af
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:55:57 +0500

    Delete exercise_7and_8.js
    Mistake

--- exercise_7and_8.js
+++
@@ -1,9 +0,0 @@
-/*n 7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result
-in the number 8. Be sure to enclose your operations in print statements to see the results.
-
-8.You should create four lines that look like this:
-console.log(5 + 3)*/
-console.log("The addition of (4+4) is:" + (4 + 4));
-console.log("The subtraction of (10-2) is:" + (10 - 2));
-console.log("The multiplication of (4*2) is:" + (4 * 2));
-console.log("The division of (24/3) is:" + (24 / 3));
