da3edf7259c35dcdbb709008f60fbb85d0d0a653
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:54:42 +0500

    Delete exercise_22.ts
    Mistake

--- exercise_22.ts
+++
@@ -1,7 +0,0 @@
-/* 22.Intentional Error: If you haven’t received an array index error in one of your programs yet, 
-try to make one happen. Change an index in one of your programs to produce an index error. 
-Make sure you correct the error before closing the program.*/
-
-let _fruits = ["Apple", "Banana", "Orange"];
-console.log(_fruits[3]);
-console.log(_fruits[1]);
