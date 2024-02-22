ee718291d205d8d762cda17d4a6571c81f25ef0a
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:49:21 +0500

    Delete exercise_13.js
    mistake

--- exercise_13.js
+++
@@ -1,7 +0,0 @@
-/* 13.Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
-and make a list that stores several examples. Use your list to print a series of statements about
- these items, such as “I would like to own a Honda motorcycle.”*/
-var vehicles = ["car", "motorcycle", "bicycle", "scooter"];
-for (var i = 0; i < vehicles.length; i++) {
-    console.log("i would love to ride a " + vehicles[i] + " Someday!");
-}
