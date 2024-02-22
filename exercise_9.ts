e8f4e629df354bcba530081ddea70f1a5e711a67
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 18:37:44 +0500

    Delete exercise_9.ts

--- exercise_9.ts
+++
@@ -1,6 +0,0 @@
-/* 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, 
-create a message that reveals your favorite number. Print that message.*/
-
-let Favorite_Number=10;
-let reveal_Messag="This is my favorite number:\n";
-console.log(reveal_Messag+Favorite_Number);
