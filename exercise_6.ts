42e9580d600b86888a26974544a1d87966c2f42b
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 18:04:55 +0500

    Add files via upload

---
+++ exercise_6.ts
@@ -0,0 +1,10 @@
+/* 6.Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of 
+the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once,
+ so the whitespace around the name is displayed. Then print the name after striping the white spaces.*/
+
+ let Name="\t \n Muhammad ali \n \t";
+ console.log("Name With WhiteSpace:"+Name);
+ Name=Name.trim();
+ console.log("Name without whitespace:"+Name);
+
+
