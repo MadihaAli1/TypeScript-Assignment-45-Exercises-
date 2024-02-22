fc7606dee3b11323c41334b294d3c1f9907a006a
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 18:17:55 +0500

    Add files via upload

---
+++ exercise_21.ts
@@ -0,0 +1,12 @@
+/* 21.They think of something you could store in a TypeScript Object. Write a program that creates Objects 
+containing these items.*/
+
+let countriesInfo = [
+    { country: "pakistan", capital: "Islamabad" },
+    { country: "Japan", capital: "Tokyo" },
+    { country: "France", capital: "Paris" },
+    { country: "Italy", capital: "Rome" },
+    { country: "Unitedkingdom", capital: "London" },
+  ];
+  console.log("Countries and Capitals:", countriesInfo);
+  
