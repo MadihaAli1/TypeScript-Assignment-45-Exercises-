e8d62cb14c4ac020f25eaa3f529f6784ecbf0f5b
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 18:04:23 +0500

    Add files via upload

---
+++ exercise_4.ts
@@ -0,0 +1,7 @@
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+  Albert Einstein once said, “A person who never made a mistake never tried anything new.”*/
+
+let Auther= "Ralpah waldo emporen,";
+let Quote= " 'life is journey not a destination' ";
+console.log(Auther+Quote);
