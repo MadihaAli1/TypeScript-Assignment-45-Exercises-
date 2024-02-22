5649dc81d5ae147e8cb3acaeaa5e1a572ff798ad
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:57:31 +0500

    Delete exercise_3.js

--- exercise_3.js
+++
@@ -1,6 +0,0 @@
-/* 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase,
-uppercase, and titlecase.*/
-var person_name = "Madiha Ali";
-console.log(person_name.toLowerCase());
-console.log(person_name.toUpperCase());
-console.log(person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase());
