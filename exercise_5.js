d6265e8e18a7306b869e74a42b247b23f477f697
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 18:01:19 +0500

    Delete exercise_5.js

--- exercise_5.js
+++
@@ -1,5 +0,0 @@
-/* 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called
-famous_person. Then compose your message and store it in a new variable called message. Print your message.*/
-var famous_person = "Mark Twain,";
-var message = " 'To succeed life,youneed two things ingonrance confidence' ";
-console.log(famous_person + message);
