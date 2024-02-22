49857f215d9cd1684d93b783c20bc5c40b19f0b8
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:48:32 +0500

    Delete exercise_12.js
    mistake

--- exercise_12.js
+++
@@ -1,7 +0,0 @@
-/* 12.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
- print a message to them. The text of each message should be the same, but each message should be
-  personalized with the person’s name.*/
-var girls = ["Abrish", "Maham", "Amna", "Mariyam"];
-for (var i = 0; i < girls.length; i++) {
-    console.log("Hello\n" + girls[i] + "\nHow are you");
-}
