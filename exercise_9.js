9ba81fd0a0eda0df9bf285865ad63b0bb693675c
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:53:21 +0500

    Delete exercise_9.js
    Mistake

--- exercise_9.js
+++
@@ -1,5 +0,0 @@
-/* 9.Favorite Number: Store your favorite number in a variable. Then, using that variable,
-create a message that reveals your favorite number. Print that message.*/
-var Favorite_Number = 10;
-var reveal_Messag = "This is my favorite number:\n";
-console.log(reveal_Messag + Favorite_Number);
