8146ba82b7ea5560007489bf4e730ec00057a5bc
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-22 17:40:50 +0500

    Add files via upload

---
+++ exercise_11.ts
@@ -0,0 +1,7 @@
+/* 11.Names: Store the names of a few of your friends in a array called names.
+ Print each person’s name by accessing each element in the list, one at a time.*/
+
+ let friends_Name=["Abrish","Maham","Amna","Mariyam"];
+for(let i=0;i<friends_Name.length;i++){
+      console.log(friends_Name[i]);
+}
