becccc2adf7064d6a4f32fd8c10e1ebe746d4ca5
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-02-27 17:45:41 +0500

    Add files via upload

---
+++ excercise_01.js
@@ -0,0 +1,4 @@
+/* 1.Personal Message: Store a person’s name in a variable, and print a message to that person.
+ Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
+var Name = "Eric";
+console.log("\u201CHello ".concat(Name, ", would you like to learn some Python today?\u201D"));
