e2fd45dcf113e5c2ea1db89e207e436d921d6096
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 15:11:54 +0500

    Delete exercise_05.js

--- exercise_05.js
+++
@@ -1,6 +0,0 @@
-/* 5.Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in
-a variable called famous_person. Then compose your message and store it in a new variable
-called message. Print your message.*/
-var famous_person = "Hazrat Ali";
-var message = "Accept The Apology, Even If It Is Not Sincer";
-console.log("".concat(famous_person, " Once Said, ").concat(message));
