df3dc8f4cfe91e3b0e78cbcd67f46e06ef1009fa
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 18:31:48 +0500

    Add files via upload

---
+++ 09. Favorite Number/exercise_9.js
@@ -0,0 +1,7 @@
+/*
+9.Favorite Number: Store your favorite number in a variable. Then, using that variable,
+create a message that reveals your favorite number. Print that message.
+*/
+var favoriteNum = 18;
+var _message = ("My favorite number is:".concat(favoriteNum));
+console.log(_message);
---
+++ 09. Favorite Number/exercise_9.ts
@@ -0,0 +1,8 @@
+/* 
+9.Favorite Number: Store your favorite number in a variable. Then, using that variable,
+create a message that reveals your favorite number. Print that message.
+*/
+
+ let favoriteNum:number=18;
+ let _message:string=(`My favorite number is:${favoriteNum}`);
+ console.log(_message);
