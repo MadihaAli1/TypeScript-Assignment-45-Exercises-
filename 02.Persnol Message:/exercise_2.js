546f9caab7386ee1777434199b1e296d552dba90
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 17:09:09 +0500

    Delete 02.Persnol Message: directory

--- 02.Persnol Message:/exercise_2.js
+++
@@ -1,6 +0,0 @@
-/*
-2.Personal Message: Store a person’s name in a variable, and print a message to that person.
-Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-*/
-var Name = "Eric";
-console.log("\u201CHello ".concat(Name, ", would you like to learn some Python today?\u201D"));
--- 02.Persnol Message:/exercise_2.ts
+++
@@ -1,8 +0,0 @@
-/* 
-2.Personal Message: Store a person’s name in a variable, and print a message to that person.
-Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-*/
-
- let Name:string="Eric";
- console.log(`“Hello ${Name}, would you like to learn some Python today?”`);
- 
