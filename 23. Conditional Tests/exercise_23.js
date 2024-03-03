ae31a61808e6313f576486d85babcae924b74357
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-04 04:24:51 +0500

    Add files via upload

---
+++ 23. Conditional Tests/exercise_23.js
@@ -0,0 +1,50 @@
+/*
+23.Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your
+code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line
+evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another
+5 tests evaluate to False.
+*/
+//Making a variables
+var five = 5;
+var ten = 10;
+//Test 1
+console.log("Test 1: five is equal to 5?");
+console.log(five == 5);
+//Test 2
+console.log("\nTest 2: ten is equal to 10?");
+console.log(ten == 10);
+//Test 3
+console.log("\nTest 3: five is not equal to 10?");
+console.log(five != ten);
+//Test 4
+console.log("\nTest 4: 10 is greater then 5");
+console.log(ten > five);
+//Test 5
+console.log("\nTest 5: 5 is smaller than 10");
+console.log(five < ten);
+//Test 6
+console.log("\nTest 6: 10 is smaller than 5");
+console.log(ten < five);
+//Test 7
+console.log("\nTest 7: 5 is equal to 10");
+console.log(five == ten);
+//Test 8
+console.log("\nTest 8:10 is not equal to 10");
+console.log(ten != 10);
+//Test 9
+console.log("\nTest 9: 5 is greater than 10");
+console.log(5 < ten);
+//Test 10
+console.log("\nTest 10:100 is smaller than 50");
+console.log(100 < 50);
