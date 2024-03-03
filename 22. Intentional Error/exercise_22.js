9c0a007002c5758bb44b971ba68376064dea0494
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-04 04:23:38 +0500

    Add files via upload

---
+++ 22. Intentional Error/exercise_22.js
@@ -0,0 +1,10 @@
+/*
+ 22.Intentional Error: If you haven’t received an array index error in one of your
+ programs yet, try to make one happen. Change an index in one of your programs
+ to produce an index error. Make sure you correct the error before closing the
+ program
+ */
+var errorArray = ["A", "B", "C", "D"];
+//producing Error ! by Accessing invald index of array
+//console.log(errorArray[10]);
+console.log(errorArray[1]);
---
+++ 22. Intentional Error/exercise_22.ts
@@ -0,0 +1,14 @@
+/*
+ 22.Intentional Error: If you haven’t received an array index error in one of your
+ programs yet, try to make one happen. Change an index in one of your programs 
+ to produce an index error. Make sure you correct the error before closing the 
+ program
+ */
+
+ let errorArray:string[]=["A","B","C","D"];
+ 
+ //producing Error ! by Accessing invald index of array
+ //console.log(errorArray[10]);
+
+//Error removed
+ console.log(errorArray[1]);
