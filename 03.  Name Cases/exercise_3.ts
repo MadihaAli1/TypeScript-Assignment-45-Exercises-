c323c49564f2959cf2e8b207f669d674febe9103
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 17:20:01 +0500

    Add files via upload

---
+++ 03.  Name Cases/exercise_3.ts
@@ -0,0 +1,10 @@
+/* 
+3.Name Cases: Store a person’s name in a variable, and then print that person’s name in
+lowercase, uppercase, and titlecase.
+*/
+
+ let personName:string="madiha ali";
+
+ console.log(personName.toLowerCase());
+ console.log(personName.toUpperCase());
+ console.log(personName.replace(/\b\w/g,(char)=>char.toUpperCase()));
