08ab8c83afa5bfe61b50f1839db1d454c5973d64
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 15:14:22 +0500

    Delete exercise_10.js

--- exercise_10.js
+++
@@ -1,7 +0,0 @@
-/* 10.Adding Comments: Choose two of the programs you’ve written, and add at least one
-comment to each. If you don’t have anything specific to write because your programs
-are too simple at this point, just add your name and the current date at the top of
- each program file. Then write one sentence describing what the program does.*/
-//program written by: Madiha
-//This program will write Hello World
-console.log("Hello World");
