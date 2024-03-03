8a3fa6701232790456e6890e8b0947e3ae1c6af4
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 15:21:28 +0500

    Delete exercise_22.js

--- exercise_22.js
+++
@@ -1,9 +0,0 @@
-/*
- Intentional Error: If you haven’t received an array index error in one of your
- programs yet, try to make one happen. Change an index in one of your programs
- to produce an index error. Make sure you correct the error before closing the
- program
- */
-var numbers = [1, 2, 3, 4, 5];
-var invalidNumber = numbers[10];
-console.log("Invalid Number:", invalidNumber);
