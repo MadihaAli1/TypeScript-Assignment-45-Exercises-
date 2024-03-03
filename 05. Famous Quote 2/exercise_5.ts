8ba19be37740dd24275f5ded0ef4af18a842bfc9
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 16:54:08 +0500

    Add files via upload

---
+++ 05. Famous Quote 2/exercise_5.ts
@@ -0,0 +1,11 @@
+/* 
+5.Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in
+a variable called famous_person. Then compose your message and store it in a new variable
+called message. Print your message.
+*/
+
+let famous_person:string="Hazrat Ali";
+
+let message:string="Accept The Apology, Even If It Is Not Sincer";
+
+console.log(`${famous_person} Once Said, ${message}`);
