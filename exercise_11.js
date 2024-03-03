fe5db429c0c0d55a02eb80f845d64432cb719a72
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-03 15:21:59 +0500

    Delete exercise_11.js

--- exercise_11.js
+++
@@ -1,6 +0,0 @@
-/* 11.Names: Store the names of a few of your friends in a array called names.
-Print each person’s name by accessing each element in the list, one at a time.*/
-var names = ["Abrish", "Hurmat", "Maliiha"];
-for (var i = 0; i < names.length; i++) {
-    console.log(names[i]);
-}
