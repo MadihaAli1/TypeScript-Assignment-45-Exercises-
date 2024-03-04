ec796f8a8ff35fb2047bc753cdf3715993ef957b
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-04 23:12:32 +0500

    Add files via upload

---
+++ 39. City Names/exercise_39.js
@@ -0,0 +1,18 @@
+/*
+39.City Names: Write a function called city_country() that takes in the name of a
+city and its country. The function should return a string formatted like this:
+
+"Lahore, Pakistan"
+
+Call your function with at least three city-country pairs, and print the value
+that’s returned
+*/
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+var location1 = city_country("Lahore", "Pakistan");
+var location2 = city_country("New York", "USA");
+var location3 = city_country("Tokyo", "Japan");
+console.log(location1);
+console.log(location2);
+console.log(location3);
